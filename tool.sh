9afce1404667de978a7e0464a7d527a80d542fe5
Mr.Paint <50089388+PaintIsPainting@users.noreply.github.com>
2019-05-04 09:12:23 +0800

    Add files via upload

---
+++ tool.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+lightgreen='\033[1;32m'
+green='\033[0;34'
+blue='\033[0;34m'
+okegreen='\033[92m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+white='\033[1;37m'
+cyan='\033[0;36m'
+purple='\033[35;1m'
+sleep 1
+
+echo "≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ "| lolcat
+echo "\033[1;31mAuthor        : 🖕《㋛₥r. ㄕaIהτ㋡》🖕      ≈ "
+echo "\033[0;34mNama Tools    : Mr.Paint Tools              ≈ "
+echo "\033[1;32mJenis Tools   : Bahan-bahan                   ≈ "
+echo "\033[1;37mKontak WA     : 082293825257                ≈ "
+echo "\033[36mPesan Gua     : HATI-HATI DALAM BERTINDAK   ≈ "
+echo "\033[0;92mRilis         : 24 April 2019               ≈ "
+echo "≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ "| lolcat
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $red"-.Yee. Knapa d'klik lw sdh ada :( "   |lolcat
+echo $cyan"-.Hati-hati Di Jalan boss :("   |lolcat
+sleep 1
+clear
+echo ""
+echo $white"-.Kejarlah Mimpimu Sampai Ke Atas Langit"   |lolcat
+echo $red"-.Artinya Dari Apa Yg Lu Inginkan"  |lolcat
+echo $green"-.Jadi Kenyataan"   |lolcat
+echo $purple"-.Sayonara :)"  |lolcat
+sleep 1
+clear
+exit
+}
+
+toilet -f slant -F gay "MR.Paint" | lolcat
+echo "\033[1;33m"
+echo "#»===============================================«#" | lolcat
+echo $red "1.#Bahan-bahan"
+echo "#»===============================================«#" | lolcat
+echo $white "2.upgrade tool"
+echo "#»===============================================«#" | lolcat
+echo $blue "00.#Exit"
+echo "##===============================================##" | lolcat
+echo "#By_Mr.Paint_" | lolcat
+read -p "<<Doa_Dlu>>≈" sob
+
+if [ $sob = 1 ] || [ $sob = 1 ]
+then
+clear
+toilet -f small -F gay "Mr.PaintIsPainting" | lolcat
+echo $blue" NGINSTALL SAYAANNGG "
+sleep 1
+pkg update
+pkg upgrade
+apt install sh
+apt install python
+apt install python2
+pip install --upgrade pip
+pip install --upgrade pip2
+apt install git
+apt install ruby
+apt install toilet
+pkg install curl
+pkg install libcurl
+pkg install openssh
+pkg install gem
+pkg install wget
+pkg install php
+pip2 install -r requipment
+pip2 install request
+pip2 install requerements
+pip2 install lolcat
+pip2 install termcolor
+apt install php
+pip2 install reqipments
+ruby install figlet
+sh Mr.Paint.sh
+echo $blue" Selesai Boss "
+fi
+
+if [ $sob = 2 ] || [ $sob = 2 ]
+then
+clear
+echo "UPGRADING" | lolcat
+apt update
+apt upgrade
+pip install --upgrade pip
+pip install --upgrade pip2
+sh Mr.Paint.sh
+echo "Done" | lolcat
+fi
+
+if [ $sob = 00 ] || [ $sob = 00 ]
+then
+clear
+echo "By Mr.Paint≈" | lolcat
+echo "Sudah Nemu Yg Di Cari Boss?" | lolcat
+echo "Sorry Lw Kurang Lengkap" | lolcat
+echo "Gua Hnya Manusia Biasa" | lolcat
+echo "Dear Jomblo" | lolcat
+echo "Manfaatin Kejombloan Kalian" | lolcat
+echo "Karna Klau Lu Pacaran, Smua IndraLu Di Rampas" | lolcat
+echo "SeeYou Next Time" | lolcat
+clear
+exit
+fi
